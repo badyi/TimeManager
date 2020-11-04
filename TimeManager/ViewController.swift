f88ba901691b3a2451ee66603b2eba17ae199f1a
badyi <badyi7@icloud.com>
2020-11-04 20:00:53 +0300

    Initial Commit

---
+++ TimeManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeManager
+//
+//  Created by и on 04.11.2020.
+//  Copyright © 2020 badyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
